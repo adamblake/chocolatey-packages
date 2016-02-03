ea5d780782d8be63460c84dc7cecd693f26ed683
Adam Blake <adamblake@g.ucla.edu>
2016-02-02 18:07:48 -0800

    Adds jasp.nuspec, chocolateyinstall.ps1, and chocolateyuninstall.ps1

---
+++ jasp/tools/chocolateyinstall.ps1
@@ -0,0 +1,45 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'jasp'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://static.jasp-stats.org/JASP-0.7.1.12-Setup.exe'
+$url64      = $url
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+
+  silentArgs    = "/qn /norestart /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/S'
+
+  softwareName  = 'JASP*'
+  checksum      = '5154cdb7a75118639908a189e45ef667'
+  checksumType  = 'md5'
+  checksum64    = '5154cdb7a75118639908a189e45ef667'
+  checksumType64= 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
