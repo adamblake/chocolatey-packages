8b366ce817882515294d17b7aea60b2ef76a0a6c
Adam Blake <adamblake@g.ucla.edu>
2016-03-28 16:32:07 -0700

    Updates installer to work with ketarin/autoinstall

--- packages/jasp/tools/chocolateyinstall.ps1
+++ packages/jasp/tools/chocolateyinstall.ps1
@@ -5,4 +5,3 @@ $url64 = '{{DownloadUrlx64}}'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
-Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$url64"
