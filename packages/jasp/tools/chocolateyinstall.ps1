d801773409da9338dbf858656c8b45d4db072011
Adam Blake <adamblake@g.ucla.edu>
2016-03-28 14:59:12 -0700

    Autoinstaller changes

--- packages/jasp/tools/chocolateyinstall.ps1
+++ packages/jasp/tools/chocolateyinstall.ps1
@@ -1,25 +1,8 @@
-﻿$ErrorActionPreference = 'Stop';
-
-$packageName= 'jasp'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://static.jasp-stats.org/JASP-0.7.1.12-Setup.exe'
-$url64      = $url
-
-$packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  url           = $url
-  url64bit      = $url64
-
-  silentArgs   = '/S'
-  validExitCodes= @(0)
-
-  softwareName  = 'JASP*'
-  checksum      = '5154cdb7a75118639908a189e45ef667'
-  checksumType  = 'md5'
-  checksum64    = '5154cdb7a75118639908a189e45ef667'
-  checksumType64= 'md5'
-}
-
-Install-ChocolateyPackage @packageArgs
+﻿$packageName = '{{PackageName}}'
+$installerType = 'exe'
+$url = '{{DownloadUrl}}'
+$url64 = '{{DownloadUrlx64}}'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$url64"
