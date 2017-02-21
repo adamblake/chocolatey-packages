6f1300d3c07678a9cf0301f3b47877a75f2a670f
Adam Blake <theadamattack@gmail.com>
2017-02-21 14:45:06 -0800

    gpower and jasp now named to allow for package fix version notation

--- gpower/update.ps1
+++ gpower/update.ps1
@@ -19,6 +19,7 @@ function global:au_GetLatest {
   $rel_uri = $download_page.links | ? href -match $re | select -First 1 -expand href
   $uri = (New-Object -TypeName 'System.Uri' -ArgumentList $releases, $rel_uri)
   $version = $uri -split '_|.zip' | select -Last 1 -Skip 1
+  $version = "$($version)00"
   return @{ Version = $version; URL32 = $uri }
 }
 
--- jasp/update.ps1
+++ jasp/update.ps1
@@ -7,7 +7,7 @@ function global:au_SearchReplace {
         'tools\chocolateyInstall.ps1' = @{
             "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^[$]version\s*=\s*)('.*')"    = "`$1'$($Latest.Version)'"
+            "(^[$]version\s*=\s*)('.*')"    = "`$1'$($Latest.check_version)'"
         }
      }
 }
@@ -18,8 +18,9 @@ function global:au_GetLatest {
   # JASP-0.8.0.0-Setup.exe
   $re = 'JASP-.+-Setup.exe$'
   $url     = $download_page.links | ? href -match $re | select -First 1 -expand href
-  $version = $url -split '-|.exe' | select -Last 1 -Skip 2
-  return @{ Version = $version; URL32 = $url }
+  $check_version = $url -split '-|.exe' | select -Last 1 -Skip 2
+  $version = "$($check_version)00"
+  return @{ Version = $version; check_version = $check_version; URL32 = $url }
 }
 
 update
