10c717faa8a3d0a852be1038c51dec463a3fbd6b
Chocolatey <chocolatey@realdimensions.net>
2017-10-23 17:55:54 +0000

    AU: 1 updated - jasp [skip ci] https://gist.github.com/cf0790e7d6a4278ee956962552c44af1/28d2118d06eff26059d1c7f9b71fe2f0e9ecabb6

--- jasp/tools/chocolateyinstall.ps1
+++ jasp/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageName  = 'jasp'
 $softwareName = 'JASP*'
 $toolsDir     = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$url32        = 'http://static.jasp-stats.org/JASP-0.8.1.2-Setup.exe'
-$checksum32   = '5ffe33d3009bfa3412264737b3f3bea1b74107dc96e0eb6388bcb848bf0605ca'
-$version      = '0.8.1.2'
+$url32        = 'https://static.jasp-stats.org/JASP-0.8.3.1-Setup.exe'
+$checksum32   = '30397afe3a948b348de1468fcd22b1aabd0210806dd13a9e76e7794e9412a0fd'
+$version      = '0.8.3.1'
 
 $fileType       = $url32 -split "\." | select -Last 1
 $silentArgs     = '/S'
