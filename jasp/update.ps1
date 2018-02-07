0e9f12ffdce8cc228d992a59d435d4cdd0c0a52b
Adam Blake <theadamattack@gmail.com>
2018-02-06 17:23:29 -0800

    Update JASP citation automatically

--- jasp/update.ps1
+++ jasp/update.ps1
@@ -2,13 +2,13 @@ import-module au
 
 $releases = 'https://jasp-stats.org/download/'
 $version_pattern = "JASP (\d+\.\d+\.\d+)"
+$current_year = Get-Date -UFormat "%Y"
 
 function global:au_GetLatest {
 	$request = Invoke-WebRequest -Uri $releases -UseBasicParsing
 	$check_version = ($request.content).tostring() | Select-String -Pattern $version_pattern | % {$_.Matches} | % {$_.Groups} | select -Last 1 | % {$_.Value}
-	$version = "$($check_version)00"
 	$url = "https://static.jasp-stats.org/JASP-$($check_version)-Setup.exe"
-	return @{ Version = $version; check_version = $check_version; URL32 = $url }
+	return @{ Version = $check_version; check_version = $check_version; URL32 = $url }
 }
 
 function global:au_SearchReplace {
@@ -17,6 +17,9 @@ function global:au_SearchReplace {
             "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
             "(^[$]version\s*=\s*)('.*')"    = "`$1'$($Latest.check_version)'"
+        }
+        'jasp.nuspec' = @{
+        	"JASP Team \(\d{4}\). JASP \(Version \d+(?:\.\d+){2,3}\)\[Computer software\]" = "JASP Team ($($current_year)). JASP (Version $($Latest.check_version))[Computer software]"
         }
      }
 }
