057fd2bfed2752dc54a491cfc68a2c47e3d84c7a
Adam Blake <theadamattack@gmail.com>
2019-05-30 17:27:40 -0700

    [jasp] Fix versioning, add x64, update description
    ### Changes:
     - Change versioning pattern matching to work with JASP, who changed their versioning scheme to use x.x.x.x
     - Change package flags to keep up with JASP, who change to using MSI installers.
     - Add links and checksums for 64 bit installers.
     - Update description to match JASP flier text better

--- jasp/tools/chocolateyinstall.ps1
+++ jasp/tools/chocolateyinstall.ps1
@@ -1,35 +1,39 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName  = 'jasp'
-$softwareName = 'JASP*'
-$toolsDir     = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$url32        = 'https://static.jasp-stats.org/JASP-0.8.5-Setup.exe'
-$checksum32   = 'c70c41e13e450588fc6530998f8eb2120e6ef8e82d178d77781db26edc56c772'
-$version      = '0.8.5'
-
-$fileType       = $url32 -split "\." | select -Last 1
-$silentArgs     = '/S'
-$validExitCodes = @(0)
+$packageName    = 'jasp'
+$softwareName   = 'JASP*'
+$toolsDir       = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$url32          = 'https://static.jasp-stats.org/JASP-0.9.2.0-32-bit.msi'
+$checksum32     = 'ebfb146f894db0b4a3994a26d9d43ef1bb2b198487fbf48755f32b4451c69298'
+$url64          = 'https://static.jasp-stats.org/JASP-0.9.2.0-64-bit.msi'
+$checksum64     = '02e8f5a285789d5b91466ec6b1545164b28cf2d33f8ba2df1722d39406ce3f24'
+$version        = '0.9.2.0'
+$fileType       = "msi"
+$silentArgs     = '/quiet'
+$validExitCodes = @(0, 3010, 1641)
 
 $packageArgs = @{
   packageName    = $packageName
+  softwareName   = $softwareName
   unzipLocation  = $toolsDir
   fileType       = $fileType
-  url            = $url32
-  softwareName   = $softwareName
-  checksum       = $checksum32
-  checksumType   = 'sha256'
   silentArgs     = $silentArgs
   validExitCodes = $validExitCodes
+  url            = $url32
+  checksum       = $checksum32
+  checksumType   = 'sha256'
+  url64bit       = $url64
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'
 }
 
 [array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
 if ($key.Count -eq 0) {
   Install-ChocolateyPackage @packageArgs
 } elseif ($key.Count -eq 1) {
-  $currentVersion = $($key.DisplayName -split " " | select -Last 1).Trim() 
+  $currentVersion = $($key.DisplayName -split " " | select -Last 1).Trim()
   if ($currentVersion -lt $version) {
-    $key | % { 
+    $key | % {
       $file = "$($_.UninstallString)"
       Uninstall-ChocolateyPackage `
         -PackageName $packageName `
--- jasp/update.ps1
+++ jasp/update.ps1
@@ -1,14 +1,15 @@
 import-module au
 
 $releases = 'https://jasp-stats.org/download/'
-$version_pattern = "JASP (\d+\.\d+\.\d+)"
+$version_pattern = "JASP (\d+\.\d+\.\d+(?:\.\d+)?)"
 $current_year = Get-Date -UFormat "%Y"
 
 function global:au_GetLatest {
 	$request = Invoke-WebRequest -Uri $releases -UseBasicParsing
 	$check_version = ($request.content).tostring() | Select-String -Pattern $version_pattern | % {$_.Matches} | % {$_.Groups} | select -Last 1 | % {$_.Value}
-	$url = "https://static.jasp-stats.org/JASP-$($check_version)-Setup.exe"
-	return @{ Version = $check_version; check_version = $check_version; URL32 = $url }
+	$url32 = "https://static.jasp-stats.org/JASP-$($check_version)-32-bit.msi"
+    $url64 = "https://static.jasp-stats.org/JASP-$($check_version)-64-bit.msi"
+	return @{ Version = $check_version; check_version = $check_version; URL32 = $url32; URL64 = $url64 }
 }
 
 function global:au_SearchReplace {
@@ -16,10 +17,13 @@ function global:au_SearchReplace {
         'tools\chocolateyInstall.ps1' = @{
             "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
             "(^[$]version\s*=\s*)('.*')"    = "`$1'$($Latest.check_version)'"
         }
         'jasp.nuspec' = @{
         	"JASP Team \(\d{4}\). JASP \(Version \d+(?:\.\d+){2,3}\)\[Computer software\]" = "JASP Team ($($current_year)). JASP (Version $($Latest.check_version))[Computer software]"
+            "\<version\>\d+\.\d+\.\d+(?:\.\d+)?" = "<version>$($Latest.check_version)"
         }
      }
 }
