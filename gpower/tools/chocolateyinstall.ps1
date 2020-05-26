0b922602f4d896c9567ddb5d9e1ed8891de19a77
Adam Blake <theadamattack@gmail.com>
2020-05-26 11:22:13 -0700

    Re-write gpower install logic
    - Clean re-write
    - Remove uninstall, autouninstaller handles it

--- gpower/tools/chocolateyinstall.ps1
+++ gpower/tools/chocolateyinstall.ps1
@@ -1,40 +1,44 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName    = 'gpower'
-$toolsDir       = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$url32          = 'http://www.gpower.hhu.de/fileadmin/redaktion/Fakultaeten/Mathematisch-Naturwissenschaftliche_Fakultaet/Psychologie/AAP/gpower/GPowerWin_3.1.9.7.zip'
+$version        = '3.1.9.7'
+$url32          = 'https://www.gpower.hhu.de/fileadmin/redaktion/Fakultaeten/Mathematisch-Naturwissenschaftliche_Fakultaet/Psychologie/AAP/gpower/GPowerWin_3.1.9.7.zip'
 $checksum32     = '653615497c763863bb10a06d605ba0073cf5f9f035bc6246e0dd21405a39bc6d'
 $checksumType   = 'sha256'
-$fileType       = 'msi'
-$silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
-$validExitCodes = @(0, 3010, 1641)
 
 # Create the temp directory and set zip file name
-$chocTempDir = $env:TEMP
-$tempDir = Join-Path $chocTempDir "$($env:chocolateyPackageName)"
-if ($env:chocolateyPackageVersion -ne $null) {$tempDir = Join-Path $tempDir "$($env:chocolateyPackageVersion)"; }
-$tempDir = $tempDir -replace '\\chocolatey\\chocolatey\\', '\chocolatey\'
-if (![System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) | Out-Null }
-$zipfile = Join-Path $tempDir "$($packageName)Archive.zip"
+$tempDir = Join-Path $env:TEMP $packageName
+If (-Not (Test-Path $tempDir -PathType 'container')) {
+  New-Item `
+    -Path $env:TEMP `
+    -Name $packageName `
+    -ItemType 'directory'
+}
 
-# Download and unzip file
+# Download the archive
+$tempZip = Join-Path $tempDir "$packageName.zip"
 Get-ChocolateyWebFile `
-  -PackageName $packageName `
-  -FileFullPath $zipfile `
-  -Url $url32 `
-  -Checksum $checksum32 `
-  -ChecksumType $checksumType
+ -PackageName $packageName `
+ -Url $url32 `
+ -FileFullPath $tempZip `
+ -Checksum $checksum32 `
+ -ChecksumType $checksumType
 
+# Extract the installer
 Get-ChocolateyUnzip `
-  -FileFullPath $zipfile `
-  -Destination $toolsDir
+  -FileFullPath $tempZip `
+  -Destination $tempDir
 
-# Install program
-$installerDir = Join-Path $toolsDir "GPower_$($env:chocolateyPackageVersion)"
-$file = Join-Path $installerDir "GPowerSetup.msi"
-Install-ChocolateyInstallPackage `
-  -PackageName $packageName `
-  -FileType $fileType `
-  -SilentArgs $silentArgs `
-  -File $file `
-  -ValidExitCodes $validExitCodes
+$installerDir = Join-Path $tempDir "GPower_$version"
+$installer = Join-Path $installerDir 'GPowerSetup.msi'
+
+# Install the application
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'msi'
+  file          = $installer
+  silentArgs    = "/qb"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
--- gpower/update.ps1
+++ gpower/update.ps1
@@ -1,10 +1,11 @@
 import-module au
 
-$releases = [System.Uri]'http://www.gpower.hhu.de/'
+$releases = [System.Uri]'https://www.gpower.hhu.de/'
 
 function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
+            "(^[$]version\s*=\s*)('.*')"    = "`$1'$($Latest.Version)'"
             "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
@@ -19,7 +20,6 @@ function global:au_GetLatest {
   $rel_uri = $download_page.links | ? href -match $re | select -First 1 -expand href
   $uri = (New-Object -TypeName 'System.Uri' -ArgumentList $releases, $rel_uri)
   $version = $uri -split '_|.zip' | select -Last 1 -Skip 1
-  $version = "$($version)00"
   return @{ Version = $version; URL32 = $uri }
 }
 
